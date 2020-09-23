39b50093653591b24f31cacab916671429225875
swagatik <swagatikamohapatra26@gmail.com>
2020-09-24 01:07:16 +0530

    search page feature code

---
+++ src/main/java/qaclickacademy/MavenJava/searchpage2.java
@@ -0,0 +1,10 @@
+package qaclickacademy.MavenJava;
+
+public class searchpage2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" the demo purpose ");
+	}
+
+}
